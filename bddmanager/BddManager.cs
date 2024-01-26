56667bd696253a4b1214184ddadb495762684475
Yuvem <matjuve33@icloud.com>
2024-01-26 16:42:06 +0100

    first commit

---
+++ bddmanager/BddManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Habilitations.bddmanager
+{
+    class BddManager
+    {
+        private static BddManager instance = null;
+        private readonly MySqlConnection connection;
+
+        private BddManager(string stringConnect)
+        {
+            connection = new MySqlConnection(stringConnect);
+            connection.Open();
+        }
+
+        public static BddManager GetInstance(string stringConnect)
+        {
+            if (instance == null)
+            {
+                instance = new BddManager(stringConnect);
+            }
+            return instance;
+        }
+
+        public void ReqUpdate(string stringQuery, Dictionary<string, object> parameters = null)
+        {
+            MySqlCommand command = new MySqlCommand(stringQuery, connection);
+            if (!(parameters is null))
+            {
+                foreach (KeyValuePair<string, object> element in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(element.Key, element.Value));
+                }
+            }
+            command.Prepare();
+            command.ExecuteNonQuery();
+        }
+        public List<Object[]> ReqSelect(string stringQuery, Dictionary<string, object> parameters = null)
+        {
+            MySqlCommand command = new MySqlCommand(stringQuery, connection);
+            if (!(parameters is null))
+            {
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+            command.Prepare();
+            MySqlDataReader reader = command.ExecuteReader();
+            int nbCols = reader.FieldCount;
+            List<Object[]> records = new List<object[]>();
+            while (reader.Read())
+            {
+                Object[] attributs = new Object[nbCols];
+                reader.GetValues(attributs);
+                records.Add(attributs);
+            }
+            reader.Close();
+            return records;
+        }
+
+    }
+
+
+
+}
+
---
+++ view/FrmHabilitations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Habilitations.view
+{
+    public partial class FrmHabilitations : Form
+    {
+        public FrmHabilitations()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
